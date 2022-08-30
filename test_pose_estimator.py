b313b70350e8cc4059c34c72cf479b89a7646bc7
fabio amadio <fabio.amadio@iit.it>
2022-08-30 16:01:26 +0200

    test file update with global registration

--- test_pose_estimator.py
+++ test_pose_estimator.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 from pose_estimator import PoseEstimator
+import open3d as o3d
+import copy
 
 yolact_weights = str(Path.home()) + "/Code/Vision/yolact/weights/yolact_plus_resnet50_54_800000.pth"
 estimator = PoseEstimator(camera_type = 'REALSENSE',
@@ -14,8 +16,13 @@ filt_params_dict = {'nb_neighbors': 100, 'std_ratio': 0.1}
 # filt_type = 'RADIUS'
 # filt_params_dict = {'nb_points': 16, 'radius': 0.0025*2.5}
 
-estimator.get_yolact_pcd(filt_type, filt_params_dict)
+obs_pcd = estimator.get_yolact_pcd(filt_type, filt_params_dict)
+obs_pcd.paint_uniform_color([0, 0, 1]) 
+gl_reg = estimator.global_registration(obs_pcd)
 
+model_glob = copy.deepcopy(estimator.model_pcd).transform(gl_reg.transformation)
+model_glob.paint_uniform_color([1, 0, 0])
+o3d.visualization.draw_geometries([model_glob, obs_pcd], window_name = 'Global registration')
 
 
 
